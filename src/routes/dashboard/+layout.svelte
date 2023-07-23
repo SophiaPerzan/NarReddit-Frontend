48aab086a133ae74a735195b03474d4c251ab42a
Sophia <sophia@perzan.dev>
2023-07-23 18:23:43 -0400

    Slight change to signin prompt text width

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -7,7 +7,7 @@
 	<slot />
 {:else}
 	<div class="flex flex-col items-center justify-center h-screen space-y-8 text-center mx-9">
-		<h1 class="text-4xl font-bold">You must be signed in to continue</h1>
+		<h1 class="text-4xl font-bold min-w-min max-w-sm">You must be signed in to continue</h1>
 		<Signin />
 	</div>
 {/if}
