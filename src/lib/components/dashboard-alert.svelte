66b32bd3e71bb3b0aa34d18ce718c9172784a8c7
Sophia <sophia@perzan.dev>
2023-07-25 20:08:26 -0400

    progressively enhanced alerts on form submission

---
+++ src/lib/components/dashboard-alert.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let type: 'success' | 'error' | 'warning' | 'info' = 'success';
+	export let content: string;
+</script>
+
+<div class="alert alert-{type} max-w-sm w-auto py-1 px-4 absolute right-4 bottom-2 flex">
+	<slot />
+	<span>{content}</span>
+</div>
--- src/routes/dashboard/create/+page.svelte
+++ src/routes/dashboard/create/+page.svelte
@@ -1,15 +1,30 @@
 <script lang="ts">
 	import type { ActionData, PageData } from './$types';
 	import { enhance } from '$app/forms';
+	import { fade, slide } from 'svelte/transition';
+	import DashboardAlert from '$lib/components/dashboard-alert.svelte';
 	export let data: PageData;
 	export let form: ActionData;
-	let languages: string[];
+	let loading = false;
+	let showAlert = false;
+
+	$: if (form) {
+		loading = false;
+		showAlert = true;
+		setTimeout(() => {
+			showAlert = false;
+		}, 7500);
+	}
 </script>
 
 <form
 	method="POST"
 	action="?/create"
 	use:enhance
+	on:submit={(e) => {
+		e.preventDefault();
+		loading = true;
+	}}
 	class="form-control w-full max-w-sm items-center gap-y-4 text-base-content"
 >
 	<div class="relative">
@@ -66,35 +81,35 @@
 	<div>
 		<h3 class="text-2xl my-2">Languages</h3>
 		<label class="label cursor-pointer nd">
-			<input type="checkbox" checked class="checkbox checkbox-sm" />
+			<input type="checkbox" name="LANGUAGES" checked class="checkbox checkbox-sm" />
 			<span class="label-text">English</span>
 		</label>
 		<label class="label cursor-pointer nd">
-			<input type="checkbox" class="checkbox checkbox-sm" />
+			<input type="checkbox" name="LANGUAGES" class="checkbox checkbox-sm" />
 			<span class="label-text">Spanish</span>
 		</label>
 		<label class="label cursor-pointer nd">
-			<input type="checkbox" class="checkbox checkbox-sm" />
+			<input type="checkbox" name="LANGUAGES" class="checkbox checkbox-sm" />
 			<span class="label-text">French</span>
 		</label>
 		<label class="label cursor-pointer nd">
-			<input type="checkbox" class="checkbox checkbox-sm" />
+			<input type="checkbox" name="LANGUAGES" class="checkbox checkbox-sm" />
 			<span class="label-text">Italian</span>
 		</label>
 		<label class="label cursor-pointer nd">
-			<input type="checkbox" class="checkbox checkbox-sm" />
+			<input type="checkbox" name="LANGUAGES" class="checkbox checkbox-sm" />
 			<span class="label-text">German</span>
 		</label>
 		<label class="label cursor-pointer nd">
-			<input type="checkbox" class="checkbox checkbox-sm" />
+			<input type="checkbox" name="LANGUAGES" class="checkbox checkbox-sm" />
 			<span class="label-text">Portuguese</span>
 		</label>
 		<label class="label cursor-pointer nd">
-			<input type="checkbox" class="checkbox checkbox-sm" />
+			<input type="checkbox" name="LANGUAGES" class="checkbox checkbox-sm" />
 			<span class="label-text">Polish</span>
 		</label>
 		<label class="label cursor-pointer nd">
-			<input type="checkbox" class="checkbox checkbox-sm" />
+			<input type="checkbox" name="LANGUAGES" class="checkbox checkbox-sm" />
 			<span class="label-text">Hindi</span>
 		</label>
 	</div>
@@ -102,20 +117,28 @@
 		<input type="submit" value="Create!" class="btn btn-outline btn-wide my-4" />
 	</div>
 </form>
-{#if form?.id}
-	<div class="alert alert-success max-w-sm w-auto py-1 px-4 absolute right-4 bottom-2">
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			class="stroke-current shrink-0 h-6 w-6"
-			fill="none"
-			viewBox="0 0 24 24"
-			><path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				stroke-width="2"
-				d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-			/></svg
+{#if loading}
+	<div in:fade out:fade>
+		<DashboardAlert content="Awaiting response from server" type="info"
+			><span class="loading loading-spinner loading-xs" /></DashboardAlert
 		>
-		<span>Success! ID: {form.id}</span>
+	</div>
+{/if}
+{#if form?.id && showAlert}
+	<div in:fade={{ delay: 600 }} out:fade>
+		<DashboardAlert content="Success! ID: {form.id}" type="success"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="stroke-current shrink-0 h-6 w-6"
+				fill="none"
+				viewBox="0 0 24 24"
+				><path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+				/></svg
+			></DashboardAlert
+		>
 	</div>
 {/if}
