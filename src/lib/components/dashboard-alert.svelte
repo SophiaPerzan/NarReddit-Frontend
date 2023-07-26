a702715dd4194dfb0a56b2d7838d0dab6c1410da
Sophia <sophia@perzan.dev>
2023-07-25 20:26:55 -0400

    fixed conditional classes of type info, error, et.

--- src/lib/components/dashboard-alert.svelte
+++ src/lib/components/dashboard-alert.svelte
@@ -3,7 +3,13 @@
 	export let content: string;
 </script>
 
-<div class="alert alert-{type} max-w-sm w-auto py-1 px-4 absolute right-4 bottom-2 flex">
+<div
+	class="alert max-w-sm w-auto py-1 px-4 absolute right-4 bottom-2 flex"
+	class:alert-success={type === 'success'}
+	class:alert-error={type === 'error'}
+	class:alert-info={type === 'info'}
+	class:alert-warning={type === 'warning'}
+>
 	<slot />
 	<span>{content}</span>
 </div>
