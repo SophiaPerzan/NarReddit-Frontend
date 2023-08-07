b94b86e9b4a746ae945e50c04de72362a5ab8a15
Sophia <sophia@perzan.dev>
2023-08-07 15:50:25 -0400

    Made footer responsive

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -2,12 +2,15 @@
 	import Icon from '@iconify/svelte';
 </script>
 
-<footer class="footer items-center p-4 bg-neutral text-neutral-content mt-8">
-	<div class="items-center grid-flow-col">
-		<h3 class="text-xl font-bold mr-4">NarReddit</h3>
-		<p class="font-medium">Copyright © 2023 - All right reserved</p>
-	</div>
-	<div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+<footer>
+	<div
+		class="relative flex flex-row p-4 bg-neutral text-neutral-content mt-8 text-center gap-x-3 min-[560px]:items-center justify-center min-[560px]:justify-between"
+	>
+		<h3 class="text-xl font-bold max-[560px]:mb-3">NarReddit</h3>
+		<div class="font-medium min-w-fit absolute min-[560px]:static bottom-2 left-0 right-0">
+			<p class="text-md max-[560px]:text-sm">Copyright © 2023 - All rights reserved</p>
+		</div>
+
 		<a href="https://discord.gg/vsgYDkqr8H"><Icon class="text-2xl" icon="logos:discord" /></a>
 	</div>
 </footer>
