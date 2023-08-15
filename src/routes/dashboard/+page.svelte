83f3d9fca995ea1acdba76920e03c9f96f0db034
Sophia <sophia@perzan.dev>
2023-08-15 17:01:40 -0400

    message to join discord on dashboard

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,3 +4,13 @@
 <p>Go to create to make videos</p>
 <p>Go to backgrounds to upload custom background videos</p>
 <p>Go to history to see your videos</p>
+<hr class="border-base-content text-base-content w-full max-w-md" />
+
+<div>
+	<h6 class="font-semibold text-xl mb-2">Consider joining the discord!</h6>
+	<ul>
+		<li>Get notifications for updates and beta features</li>
+		<li>Get support for issues</li>
+		<li>Recommend new features to add!</li>
+	</ul>
+</div>
