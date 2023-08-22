56803ffc338f20b6b58900db9c8936f95d747ed1
Sophia <sophia@perzan.dev>
2023-08-22 19:24:43 -0400

    Made hero video play on safari

--- src/lib/components/hero-video.svelte
+++ src/lib/components/hero-video.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
 	export let videoUrl: string;
-	export let videoUrlHQ: string; //1080p progressively loaded video. TODO
+	export let videoUrlAlt: string;
 </script>
 
 <div class="hero min-h-screen relative overflow-hidden">
-	<video autoplay loop muted class="absolute z-0 w-auto min-w-full min-h-full max-w-none">
-		<source src={videoUrl} type="video/webm" />
+	<video
+		autoplay
+		loop
+		muted
+		playsinline
+		disablepictureinpicture
+		preload="auto"
+		class="absolute z-0 w-auto min-w-full min-h-full max-w-none"
+	>
+		<source src={videoUrl} type="video/mp4" />
+		<source src={videoUrlAlt} type="video/mp4" />
 		Your browser does not support the video tag.
 	</video>
 	<div class="hero-overlay bg-opacity-80 absolute z-10 w-full h-full" />
